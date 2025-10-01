2785551614d29851dba406ffcd340fbbdcd223d5
Clipped & Viral <clippedviral80@gmail.com>
2025-10-01 00:20:57 +0000

    "https://botup.com/images/blog/ai-chatbot-app.png?v=1685597433119865848"

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
+        protocol: 'https' ,
+        hostname: 'botup.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
         protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
